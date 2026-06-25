f2a2dc6ae7e69bba932eccd1745bec5cd32e74dc
Arthur Noreika <arthurnoreika11@live.com>
2026-06-25 20:43:25 +0100

    update masterMatter constructor to include status and jurisdiction; modify matterList entries accordingly

--- misc/matterList.js
+++ misc/matterList.js
@@ -1,18 +1,20 @@
-function masterMatter(cID, mID, areaOfLaw) {
+function masterMatter(cID, mID, areaOfLaw, mStatus, mJurisdiction) {
     this.clientID = cID;
     this.matterID = mID;
     this.matterAreaOfLaw = areaOfLaw;
-    this.information = function () {
-        return "Client ID: " + this.clientID + ", Matter ID: " + this.matterID + ", Area of Law: " + this.matterAreaOfLaw;
+    this.matterStatus = mStatus;
+    this.jurisdiction = mJurisdiction;
+    this.getMatterStatus = function () {
+        return this.matterStatus;
     }
 }
 
 const matterList = [
-    new masterMatter(1, 101, "Family Law"),
-    new masterMatter(2, 102, "Criminal Law"),
-    new masterMatter(3, 103, "Corporate Law"),
-    new masterMatter(4, 104, "Intellectual Property Law"),
-    new masterMatter(5, 105, "Employment Law")
+    new masterMatter(1, 101, "Family Law", "Open", "England"),
+    new masterMatter(2, 102, "Criminal Law", "Closed", "Wales"),
+    new masterMatter(3, 103, "Corporate Law", "In Progress", "Scotland"),
+    new masterMatter(4, 104, "Intellectual Property Law", "Open", "Northern Ireland"),
+    new masterMatter(5, 105, "Employment Law", "Closed", "Wales")
 ];
 
 module.exports = matterList;
