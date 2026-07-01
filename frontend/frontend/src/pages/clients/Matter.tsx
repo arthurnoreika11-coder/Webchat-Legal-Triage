4c5ac19c2f187cc0995bcaca7e723bf29bb30827
arthurnoreika11-coder <arthurnoreika11@live.com>
2026-07-01 14:00:55 +0000

    feat: add Matter component with initial state setup

---
+++ frontend/frontend/src/pages/clients/Matter.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function Matter() {
+    const matterData = useState ({
+        subject: String,
+        enquiryType: String,
+        message: String
+    });
+
+return (
+    <main className= 'matterContainer'> 
+        <h1 className= 'matterTitle'> </h1>
+        
+    </main>
+);
+
+}
